793595700e08199b4675980c39c2edc8902e3987
bradyliao <bradyliao123@gmail.com>
2020-10-12 17:43:30 +0800

    input module

--- DS_project_1/main.cpp
+++ DS_project_1/main.cpp
@@ -6,9 +6,122 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <queue>
+
+using namespace std ;
+
+
+class block
+{
+public:
+   block(string type, int initialColumn, int LRmove)
+   {
+      this-> type = type ;
+      this-> initialColumn = initialColumn ;
+      this-> LRmove = LRmove ;
+   }
+   
+   string getType()
+   {
+      return type ;
+   }
+   
+   int getInitialColumn()
+   {
+      return initialColumn ;
+   }
+   
+   int getLRmove()
+   {
+      return LRmove ;
+   }
+   
+private:
+   string type ;
+   int initialColumn, LRmove ;
+   
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void fileInput(string fileName, int &row, int &column, queue<block> &blocksQueue){
+   string currentBlockType ;
+   int currentBlockInitialColumn, currentBlockLRmove ;
+   
+   ifstream input(fileName) ;
+   
+   input >> row >> column ;
+   
+   while(input){
+      input >> currentBlockType >> currentBlockInitialColumn >> currentBlockLRmove ;
+      if (currentBlockType == "End")
+         break ;
+      block currentInputBlock = block(currentBlockType, currentBlockInitialColumn, currentBlockLRmove) ;
+      blocksQueue.push(currentInputBlock) ;
+      
+      /*
+       test input block
+       cout << blocksQueue.back().getType() << endl ;
+       */
+   }
+   
+   
+   
+   
+}
+
+
+
+
 
 int main(int argc, const char * argv[]) {
-   // insert code here...
-   std::cout << "Hello, World!\n";
+   int boardRow, boardColumn ;
+   queue<block> blocksQueue ;
+   
+   if (argc == 2) {
+      fileInput(argv[1], boardRow, boardColumn, blocksQueue) ;
+      //test input
+      cout << boardRow << ' ' << boardColumn << endl ;
+      
+      while(!blocksQueue.empty()){
+         cout << blocksQueue.front().getType() << " " << blocksQueue.front().getInitialColumn() << " " << blocksQueue.front().getLRmove() << endl ;
+         blocksQueue.pop() ;
+      }
+   }
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
    return 0;
 }
