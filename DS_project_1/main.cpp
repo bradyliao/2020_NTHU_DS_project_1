8038cf3b8be03ac38e7e590ee648e65c9925787a
bradyliao <bradyliao123@gmail.com>
2020-10-13 01:31:08 +0800

    game board / assignBlockArray

--- DS_project_1/main.cpp
+++ DS_project_1/main.cpp
@@ -37,26 +37,110 @@ public:
       return LRmove ;
    }
    
+   void assignBlockArray()
+   {
+      if (type == "T1")
+      {
+         bool miniBlock[4][4] = {{0,1,0,0}, {1,1,1,0}, {0,0,0,0}, {0,0,0,0}} ;
+      }
+      
+      if (type == "T2")
+      {
+         bool miniBlock[4][4] = {{0,1,0,0}, {1,1,0,0}, {0,1,0,0}, {0,0,0,0}} ;
+      }
+      
+      if (type == "T3")
+      {
+         bool miniBlock[4][4] = {{1,1,1,0}, {0,1,0,0}, {0,0,0,0}, {0,0,0,0}} ;
+      }
+      
+      if (type == "T4")
+      {
+         bool miniBlock[4][4] = {{1,0,0,0}, {1,1,0,0}, {1,0,0,0}, {0,0,0,0}} ;
+      }
+      
+      if (type == "L1")
+      {
+         bool miniBlock[4][4] = {{1,1,0,0}, {1,0,0,0}, {1,0,0,0}, {0,0,0,0}} ;
+      }
+      
+      if (type == "L2")
+      {
+         bool miniBlock[4][4] = {{1,0,0,0}, {1,1,1,0}, {0,0,0,0}, {0,0,0,0}} ;
+      }
+      
+      if (type == "L3")
+      {
+         bool miniBlock[4][4] = {{0,1,0,0}, {0,1,0,0}, {1,1,0,0}, {0,0,0,0}} ;
+      }
+      
+      if (type == "L4")
+      {
+         bool miniBlock[4][4] = {{1,1,1,0}, {0,0,1,0}, {0,0,0,0}, {0,0,0,0}} ;
+      }
+      
+      if (type == "J1")
+      {
+         <#statements#>
+      }
+      
+      if (type == "J2")
+      {
+         <#statements#>
+      }
+      
+      if (type == "J3")
+      {
+         <#statements#>
+      }
+      
+      if (type == "J4")
+      {
+         <#statements#>
+      }
+      
+      if (type == "S1")
+      {
+         <#statements#>
+      }
+      
+      if (type == "S2")
+      {
+         <#statements#>
+      }
+      
+      if (type == "Z1")
+      {
+         <#statements#>
+      }
+      
+      if (type == "Z2")
+      {
+         <#statements#>
+      }
+      
+      if (type == "I1")
+      {
+         <#statements#>
+      }
+      
+      if (type == "I2")
+      {
+         <#statements#>
+      }
+      
+      if (type == "O")
+      {
+         <#statements#>
+      }
+      
+   }
+   
 private:
    string type ;
    int initialColumn, LRmove ;
-   
+   bool miniBlock[4][4] ;
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -76,6 +160,30 @@ void fileInput(string fileName, int &row, int &column, queue<block> &blocksQueue
       blocksQueue.push(currentInputBlock) ;
    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -102,8 +210,15 @@ int main(int argc, const char * argv[]) {
    }
    
 
+   
    // create game board
-   bool gameBoar[boardRow+1][boardColumn+1];
+   bool gameBoard[boardRow+1][boardColumn+1];
+   
+   for (int i = 0; i <= boardRow; i++)
+      for (int j = 0; j <= boardColumn; j++)
+         gameBoard[i][j] = false ;
+      
+   
    
    
    
