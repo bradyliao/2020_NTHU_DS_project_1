8d6845279a12af2306f82150b854b7c6ec196cb0
bradyliao <bradyliao123@gmail.com>
2020-10-15 20:22:07 +0800

    testing GitHub

--- DS_project_1/main.cpp
+++ DS_project_1/main.cpp
@@ -312,7 +312,7 @@ void displayBoardWithBoundary(bool **gameBoard, int boardRow, int boardColumn)
    }
 }
 
- 
+
 // check over upper bound (over->true, within bound->false)
 bool checkOverUpperBound(bool **gameBoard, int boardRow, int boardColumn)
 {
@@ -452,3 +452,4 @@ int main(int argc, const char * argv[]) {
    
    return 0;
 }
+//testing github
